00abea46c0d8ea50ab72633e229d3683b2aa8690
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-02 10:05:47 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "West Indies",
-         	nation2: "Sri Lanka",
-         	date: "Mar 22-26, 2008",
-         	venue: "Providence",
-         	score: "Sri Lanka 476/8d & 240/7d, West Indies (T: 437) 280 & 315; Sri Lanka won by 121 runs",
-         	crickdlequestion: "Sri Lanka player scored 54* and took 3/48 & 5/61 in this match?"
+         	nation1: "Bangladesh",
+         	nation2: "Zimbabwe",
+         	date: "Feb 22-25, 2020",
+         	venue: "Mirpur",
+         	score: "Zimbabwe 265 & 189, Bangladesh 560/6d; Bangladesh won by an innings and 106 runs",
+         	crickdlequestion: "Bangladesh player scored 203* in this match?"
          };
          
-         const correctPlayer = ["VAAS"];
+         const correctPlayer = ["RAHIM"];
