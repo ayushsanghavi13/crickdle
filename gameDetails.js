1615641d0c42b47a8f0ae598c13cb4ab7a79811c
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-07 09:58:48 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "South Africa",
-         	nation2: "England",
-         	date: "Jan 3-7, 2020",
-         	venue: "Cape Town",
-         	score: "England 269 & 391/8d, South Africa (T: 438) 223 & 248; England won by 189 runs",
-         	crickdlequestion: "England player scored 135* and took 6 catches in this match?"
+         	nation1: "Pakistan",
+         	nation2: "Sri Lanka",
+         	date: "Dec 19-23, 2019",
+         	venue: "Karachi",
+         	score: "Pakistan 191 & 555/3d, Sri Lanka (T: 476) 271 & 212; Pakistan won by 263 runs",
+         	crickdlequestion: "Pakistan player scored 38 & 174 in this match?"
          };
          
-         const correctPlayer = ["STOKES"];
+         const correctPlayer = ["ALI"];
