90182f799b8562ec716ce9fb101f86016d6ef39d
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-08 09:58:31 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "Pakistan",
-         	nation2: "Sri Lanka",
-         	date: "Dec 19-23, 2019",
-         	venue: "Karachi",
-         	score: "Pakistan 191 & 555/3d, Sri Lanka (T: 476) 271 & 212; Pakistan won by 263 runs",
-         	crickdlequestion: "Pakistan player scored 38 & 174 in this match?"
+         	nation1: "South Africa",
+         	nation2: "Australia",
+         	date: "Mar 16-18, 2006",
+         	venue: "Cape Town",
+         	score: "South Africa 205 & 197, Australia (T: 95) 308 & 95/3; Australia won by 7 wickets",
+         	crickdlequestion: "Australia player took 5/55 & 4/34 in this match?"
          };
          
-         const correctPlayer = ["ALI"];
+         const correctPlayer = ["CLARK"];
