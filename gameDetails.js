5fc333e3fdabee55ce348b4be697f996c6aa3392
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-11 09:58:34 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "Sri Lanka",
-         	nation2: "Zimbabwe",
-         	date: "Jan 12-15, 2002",
-         	venue: "Galle",
-         	score: "Sri Lanka 418 & 212/2d, Zimbabwe (T: 395) 236 & 79; Sri Lanka won by 315 runs",
-         	crickdlequestion: "Sri Lanka player scored 28 & 36 and took 5/43 & 4/31 in this match?"
+         	nation1: "England",
+         	nation2: "New Zealand",
+         	date: "May 16-19, 2013",
+         	venue: "Lord's",
+         	score: "England 232 & 213, New Zealand (T: 239) 207 & 68; England won by 170 runs",
+         	crickdlequestion: "England player scored 26* and took 1/64 & 7/44 in this match?"
          };
          
-         const correctPlayer = ["JAYASURIYA"];
+         const correctPlayer = ["BROAD"];
