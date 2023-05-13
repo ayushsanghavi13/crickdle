98c4f6a472741504302107304449848e8dbd34a9
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-13 10:13:49 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "Australia",
-         	nation2: "India",
-         	date: "Dec 14-18, 2018",
-         	venue: "Perth",
-         	score: "Australia 326 & 243, India (T: 287) 283 & 140; Australia won by 146 runs",
-         	crickdlequestion: "Australia player took 5/67 & 3/39 in this match?"
+         	nation1: "West Indies",
+         	nation2: "England",
+         	date: "Feb 26-Mar 2, 2009",
+         	venue: "Bridgetown",
+         	score: "England 600/6d & 279/2d, West Indies 749/9d; Match drawn",
+         	crickdlequestion: "West Indies player scored 291 in this match?"
          };
          
-         const correctPlayer = ["LYON"];
+         const correctPlayer = ["SARWAN"];
