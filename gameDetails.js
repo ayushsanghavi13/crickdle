d62973beb721dda17965e3b55ef90741bc2c7627
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-14 10:50:37 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "West Indies",
-         	nation2: "England",
-         	date: "Feb 26-Mar 2, 2009",
-         	venue: "Bridgetown",
-         	score: "England 600/6d & 279/2d, West Indies 749/9d; Match drawn",
-         	crickdlequestion: "West Indies player scored 291 in this match?"
+         	nation1: "England",
+         	nation2: "India",
+         	date: "Aug 9-13, 2007",
+         	venue: "The Oval",
+         	score: "India 664 & 180/6d, England (T: 500) 345 & 369/6; Match drawn",
+         	crickdlequestion: "India player scored 110* and took 3/94 & 2/123 in this match?"
          };
          
-         const correctPlayer = ["SARWAN"];
+         const correctPlayer = ["KUMBLE"];
