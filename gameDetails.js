c5c65e4279574a63b3665b75dbc8a875dde07144
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-15 09:58:21 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "England",
-         	nation2: "India",
-         	date: "Aug 9-13, 2007",
-         	venue: "The Oval",
-         	score: "India 664 & 180/6d, England (T: 500) 345 & 369/6; Match drawn",
-         	crickdlequestion: "India player scored 110* and took 3/94 & 2/123 in this match?"
+         	nation1: "West Indies",
+         	nation2: "New Zealand",
+         	date: "Jul 25-29, 2012",
+         	venue: "North Sound",
+         	score: "New Zealand 351 & 272, West Indies (T: 102) 522 & 102/1; West Indies won by 9 wickets",
+         	crickdlequestion: "West Indies player took 5/132 & 3/91 in this match?"
          };
          
-         const correctPlayer = ["KUMBLE"];
+         const correctPlayer = ["NARINE"];
