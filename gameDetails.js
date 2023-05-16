0410933aca099b5e574e98689574fb5179671de4
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-16 09:59:34 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
          	nation1: "West Indies",
-         	nation2: "New Zealand",
-         	date: "Jul 25-29, 2012",
+         	nation2: "England",
+         	date: "Jan 31-Feb 2, 2019",
          	venue: "North Sound",
-         	score: "New Zealand 351 & 272, West Indies (T: 102) 522 & 102/1; West Indies won by 9 wickets",
-         	crickdlequestion: "West Indies player took 5/132 & 3/91 in this match?"
+         	score: "England 187 & 132, West Indies (T: 14) 306 & 17/0; West Indies won by 10 wickets",
+         	crickdlequestion: "West Indies player took 4/30 & 4/52 in this match?"
          };
          
-         const correctPlayer = ["NARINE"];
+         const correctPlayer = ["ROACH"];
