4f5b3755c4f6b4df9299b462f3c93cf118634bca
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-18 09:58:40 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "Bangladesh",
-         	nation2: "South Africa",
-         	date: "Feb 22-25, 2008",
-         	venue: "Mirpur",
-         	score: "Bangladesh 192 & 182, South Africa (T: 205) 170 & 205/5; South Africa won by 5 wickets",
-         	crickdlequestion: "South Africa player took 5/30 in this match?"
+         	nation1: "India",
+         	nation2: "West Indies",
+         	date: "Nov 22-26, 2011",
+         	venue: "Wankhede",
+         	score: "West Indies 590 & 134, India (T: 243) 482 & 242/9; Match drawn",
+         	crickdlequestion: "India player scored 103 and took 5/156 & 4/34 in this match?"
          };
          
-         const correctPlayer = ["KALLIS"];
+         const correctPlayer = ["ASHWIN"];
