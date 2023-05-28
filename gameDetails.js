db46085d7a89e0c907b09afe88f9cd52fc5a8f43
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-28 09:58:15 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "Australia",
-         	nation2: "South Africa",
-         	date: "Dec 16-20, 2005",
-         	venue: "Perth",
-         	score: "Australia 258 & 528/8d, South Africa (T: 491) 296 & 287/5; Match drawn",
-         	crickdlequestion: "Australia player scored 41 & 203* in this match?"
+         	nation1: "New Zealand",
+         	nation2: "Bangladesh",
+         	date: "Feb 28-Mar 3, 2019",
+         	venue: "Hamilton",
+         	score: "Bangladesh 234 & 429, New Zealand 715/6d; New Zealand won by an innings and 52 runs",
+         	crickdlequestion: "New Zealand player scored 200* in this match?"
          };
          
-         const correctPlayer = ["HODGE"];
+         const correctPlayer = ["WILLIAMSON"];
