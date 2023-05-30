21eb8281795c5c0f8cd342432b83d230553b567e
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-05-30 09:58:30 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
          	nation1: "Australia",
-         	nation2: "Pakistan",
-         	date: "Jan 3-7, 2017",
+         	nation2: "ICC World XI",
+         	date: "Oct 14-17, 2005",
          	venue: "Sydney",
-         	score: "Australia 538/8d & 241/2d, Pakistan (T: 465) 315 & 244; Australia won by 220 runs",
-         	crickdlequestion: "Australia player scored 113 & 55 in this match?"
+         	score: "Australia 345 & 199, ICC World XI (T: 355) 190 & 144; Australia won by 210 runs",
+         	crickdlequestion: "Australia player scored 111 & 77 in this match?"
          };
          
-         const correctPlayer = ["WARNER"];
+         const correctPlayer = ["HAYDEN"];
