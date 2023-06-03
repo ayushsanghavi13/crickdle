d1c56aa51fa24acc685a62e0b19bd308815295c4
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-06-03 06:50:48 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "Bangladesh",
-         	nation2: "India",
-         	date: "Dec 14-18, 2022",
-         	venue: "Chattogram",
-         	score: "India 404 & 258/2d, Bangladesh (T: 513) 150 & 324; India won by 188 runs",
-         	crickdlequestion: "India player scored 40 and took 5/40 & 3/73 in this match?"
-         };
+         	nation1: "Zimbabwe",
+         	nation2: "South Africa",
+         	date: "Sep 14-18, 2001",
+         	venue: "Bulawayo",
+         	score: "Zimbabwe 419/9d & 96/3, South Africa 519/8d; Match drawn",
+         	crickdlequestion: "South Africa player scored 189* in this match?"
+            };
          
          const correctPlayer = ["YADAV"];
