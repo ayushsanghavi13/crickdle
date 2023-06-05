6370aaca3530ff567e9cc2b3d3fe0ae25bac5e94
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-06-05 13:30:46 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "England",
-         	nation2: "Sri Lanka",
-         	date: "Jun 9-13, 2016",
-         	venue: "Lord's",
-         	score: "England 416 & 233/7d, Sri Lanka (T: 362) 288 & 78/1; Match drawn",
-         	crickdlequestion: "England player scored 167* & 32 in this match?"
+         	nation1: "New Zealand",
+         	nation2: "Pakistan",
+         	date: "Nov 9-13, 2014",
+         	venue: "Abu Dhabi",
+         	score: "Pakistan 566/3d & 175/2d, New Zealand (T: 480) 262 & 231; Pakistan won by 248 runs",
+         	crickdlequestion: "Pakistan player took 4/22 & 2/48 in this match?"
             };
          
-         const correctPlayer = ["BAIRSTOW"];
+         const correctPlayer = ["ALI"];
