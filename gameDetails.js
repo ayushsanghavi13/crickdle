5ab176672b39467284fa425b000a76a872fc8644
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-06-06 11:36:56 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "New Zealand",
-         	nation2: "Pakistan",
-         	date: "Nov 9-13, 2014",
-         	venue: "Abu Dhabi",
-         	score: "Pakistan 566/3d & 175/2d, New Zealand (T: 480) 262 & 231; Pakistan won by 248 runs",
-         	crickdlequestion: "Pakistan player took 4/22 & 2/48 in this match?"
+         	nation1: "Australia",
+         	nation2: "West Indies",
+         	date: "Dec 8-11, 2022",
+         	venue: "Adelaide",
+         	score: "Australia 511/7d & 199/6d, West Indies (T: 497) 214 & 77; Australia won by 419 runs",
+         	crickdlequestion: "Australia player scored 175 & 38* in this match?"
             };
          
-         const correctPlayer = ["ALI"];
+         const correctPlayer = ["HEAD"];
