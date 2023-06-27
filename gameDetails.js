bd7e5d8708fea5bff6b2010d0cfb68a63b03a428
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-06-27 12:30:17 +0200

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "India",
-         	nation2: "New Zealand",
-         	date: "Dec 3-6, 2021",
-         	venue: "Wankhede",
-         	score: "India 325 & 276/7d, New Zealand (T: 540) 62 & 167; India won by 372 runs",
-         	crickdlequestion: "India player scored 150 & 62 in this match?"
+         	nation1: "Australia",
+         	nation2: "South Africa",
+         	date: "Nov 9-13, 2012",
+         	venue: "Brisbane",
+         	score: "South Africa 450 & 166/5, Australia 565/5d; Match drawn",
+         	crickdlequestion: "Australia player scored 259* in this match?"
             };
          
-         const correctPlayer = ["AGARWAL"];
+         const correctPlayer = ["CLARKE"];
