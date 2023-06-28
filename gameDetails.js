2b75568ced9dd93846c79fb556751afe0367b098
Ayush Sanghavi <66960490+ayushsanghavi13@users.noreply.github.com>
2023-06-28 10:00:06 +0100

    Update gameDetails.js

--- gameDetails.js
+++ gameDetails.js
@@ -1,10 +1,10 @@
         const gameDetails = {
-         	nation1: "Australia",
-         	nation2: "South Africa",
-         	date: "Nov 9-13, 2012",
-         	venue: "Brisbane",
-         	score: "South Africa 450 & 166/5, Australia 565/5d; Match drawn",
-         	crickdlequestion: "Australia player scored 259* in this match?"
+         	nation1: "Sri Lanka",
+         	nation2: "Bangladesh",
+         	date: "Mar 8-12, 2013",
+         	venue: "Galle",
+         	score: "Sri Lanka 570/4d & 335/4d, Bangladesh (T: 268) 638 & 70/1; Match drawn",
+         	crickdlequestion: "Bangladesh player scored 200 in this match?"
             };
          
-         const correctPlayer = ["CLARKE"];
+         const correctPlayer = ["RAHIM"];
